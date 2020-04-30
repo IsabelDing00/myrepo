8a59c6291f49cf77b76655f740b467349a3cff60
Isabel Ding <din18008@byui.edu>
2020-04-30 00:05:40 -0600

    task2

---
+++ task2.R
@@ -0,0 +1,41 @@
+1 / 200 * 30
+
+(59 + 73 + 2) /3
+
+sin(pi/2)
+
+x <- 3 * 4
+x
+
+this_is_a_really_long_name <- 2.5
+this_is_a_really_long_name
+r_rock <- 2^3
+r_rock
+
+seq(1,10)
+
+x <- "Hello World"
+x
+
+y <- seq(1, 10, length.out = 5)
+y
+
+(y <- seq(1, 10, length.out = 5))
+y
+
+2 + 1 == 3
+
+4 + 2  >= 3
+3 + 5 <= 1
+
+(2 + 1 == 3) & (2 + 1 == 4)
+(2 + 1 == 3) | (2 + 1 == 4)
+
+seq(from = 1, to = 1)
+seq(from = 2, to = 5)
+
+c(6, 11, 13, 31, 90, 92)
+
+# I have learned what is Tidyverse and how to use functions and how those functions works. 
+# Debugging is important (learned how to result the errors and warings.) 
+
